7e7f65bab0f8cbac8cae79aa1ed6a71155f9b0ca
Oğuz Çelik <oguzcelik96@gmail.com>
2016-03-30 17:26:13 +0300

    Feature - Swipe to refresh

--- app/src/main/java/xyz/oguzcelik/vngrschallenge/MainActivity.java
+++ app/src/main/java/xyz/oguzcelik/vngrschallenge/MainActivity.java
@@ -1,12 +1,19 @@
 package xyz.oguzcelik.vngrschallenge;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.Tweet;
 import com.twitter.sdk.android.tweetui.SearchTimeline;
+import com.twitter.sdk.android.tweetui.TimelineResult;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -25,7 +32,26 @@ public class MainActivity extends AppCompatActivity {
                 .query("#android")
                 .build();
 
-        CustomTweetTimelineListAdapter adapter = new CustomTweetTimelineListAdapter(this, searchTimeline);
+        final CustomTweetTimelineListAdapter adapter = new CustomTweetTimelineListAdapter(this, searchTimeline);
+
+        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                adapter.refresh(new Callback<TimelineResult<Tweet>>() {
+                    @Override
+                    public void success(Result<TimelineResult<Tweet>> result) {
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+
+                    @Override
+                    public void failure(TwitterException e) {
+                        Toast.makeText(getApplicationContext(), "Could not load tweets. Swipe to refresh.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
         final ListView tweetsListView = (ListView) findViewById(R.id.listview_tweets);
         tweetsListView.setAdapter(adapter);
